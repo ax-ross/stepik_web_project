d17322b35cc44572dc48e3ed95f91d8f1646c1bf
ax-ross <ax-ross@yandex.ru>
2021-08-08 20:52:47 +0300

    completed 2nd step of web project

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+gunicorn --bind='0.0.0.0:8080' hello:app
