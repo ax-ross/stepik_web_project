c2a2c2a6e540e2667fa758a1e4b49bc6f8daf545
ax-ross <ax-ross@yandex.ru>
2021-08-14 06:03:43 +0300

    fixed init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn --bind='0.0.0.0:8080' hello:app
+cd ask/
+gunicorn --bind='0.0.0.0:8000' ask.wsgi:application
