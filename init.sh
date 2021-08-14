f266da590995153164eb35f99b1ef7fa3ec55695
ax-ross <ax-ross@yandex.ru>
2021-08-14 06:33:04 +0300

    fixed for 3d step

--- init.sh
+++ init.sh
@@ -1,5 +1,3 @@
-sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 cd ask/
 gunicorn --bind='0.0.0.0:8000' ask.wsgi:application
