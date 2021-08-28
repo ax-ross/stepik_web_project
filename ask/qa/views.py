217e409feed95fe555c5c210cb73cd5c3be2254b
ax-ross <ax-ross@yandex.ru>
2021-08-28 20:36:57 +0300

    started to implement views function for handling new data and generating new html page from templates

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,19 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.core.paginator import Paginator
 from .models import Question
 
+
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
 
-def quest_list_all(request):
+
+def quests_list_all(request):
     quests = Question.objects.new()
     page = request.GET.get('page', 1)
     paginator = Paginator(quests, 10)
-    paginator.baseurl =  '/?page=2'
-    page = paginarot.page(page)
-    return render(request, 'templates/quests_list.html', {
+    paginator.baseurl = '/?page='
+    page = paginator.get_page(page)
+    return render(request, 'quests_list.html', {
         'quests': page.object_list,
         'paginator': paginator,
         'page': page,
-        })
+    })
+
+
+def quests_list_popular(request):
+    quests = Question.objects.popular()
+    page = request.GET.get('page', 1)
+    paginator = Paginator(quests, 10)
+    paginator.baseurl = '/?page='
+    page = paginator.page(page)
+    return render(request, 'quests_list.html', {
+        'quests': page.object_list,
+        'paginator': paginator,
+        'page': page,
+    })
+
+
+def get_quest(request, id):
+    quest = get_object_or_404(Question, pk=id)
+    return render(request, 'quests.html', {
+        'quest': quest
+    })
