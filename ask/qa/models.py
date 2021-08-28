c76eae2ca46d99d8ee6e319cd57239e76d19d4c6
ax-ross <ax-ross@yandex.ru>
2021-08-28 20:32:55 +0300

    implemented methods get_url and __str__

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class QuestionManager(models.Manager):
@@ -9,6 +10,7 @@ class QuestionManager(models.Manager):
     def popular(self):
         return self.order_by('-rating')
 
+
 class Question(models.Model):
     objects = QuestionManager()
     title = models.CharField(max_length=255)
@@ -16,11 +18,17 @@ class Question(models.Model):
     added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User, related_name='qustion_like_user')
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+
+    def get_url(self):
+        return reverse('question', kwargs={'id': self.pk})
+
+    def __str__(self):
+        return self.title
+
 
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField()
     question = models.ForeignKey(Question, null=False, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-
