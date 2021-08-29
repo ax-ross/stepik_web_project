9771ce3eca27f94ec8d63fbdb129e6edb8a9a3cf
ax-ross <ax-ross@yandex.ru>
2021-08-29 09:43:47 +0300

    reworked pagination

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Question
 
 
@@ -8,16 +8,24 @@ def test(request, *args, **kwargs):
     return HttpResponse('OK')
 
 
-def quests_list_all(request):
-    quests = Question.objects.new()
+def paginate_question(request, qs):
+    limit = 10
     page = request.GET.get('page', 1)
-    paginator = Paginator(quests, 10)
-    paginator.baseurl = '/?page='
-    page = paginator.get_page(page)
+    paginator = Paginator(qs, limit)
+    try:
+        page = paginator.page(page)
+    except PageNotAnInteger:
+        page = paginator.page(1)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
+    return page
+
+
+def quests_list_all(request):
+    quests_list = Question.objects.new()
+    quests = paginate_question(request, quests_list)
     return render(request, 'quests_list.html', {
-        'quests': page.object_list,
-        'paginator': paginator,
-        'page': page,
+        'quests': quests,
     })
 
 
