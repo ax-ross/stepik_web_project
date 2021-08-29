0a803cf7bd39180d1e67599dbc9ea6d9b13521c3
ax-ross <ax-ross@yandex.ru>
2021-08-29 08:29:20 +0300

    changed database for deploy

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'qa.apps.QaConfig',
-    'django_seed',
 ]
 
 MIDDLEWARE = [
@@ -79,8 +78,9 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR + '/' 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db',
+        'USER': 'root',
     }
 }
 
