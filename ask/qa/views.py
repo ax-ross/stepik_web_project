f585cccd47d521d02f9f454b4022349a329f1e95
ax-ross <ax-ross@yandex.ru>
2021-08-29 13:51:18 +0300

    remade paginate func and quest replaced to question

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage
 from .models import Question
 
 
@@ -8,42 +8,39 @@ def test(request, *args, **kwargs):
     return HttpResponse('OK')
 
 
-def paginate_question(request, qs):
+def paginate(request, qs):
     limit = 10
-    page = request.GET.get('page', 1)
+    try:
+        page = int(request.GET.get('page', 1))
+    except ValueError:
+        raise Http404
     paginator = Paginator(qs, limit)
+    paginator.baseurl = '/?page='
     try:
         page = paginator.page(page)
-    except PageNotAnInteger:
-        page = paginator.page(1)
     except EmptyPage:
         page = paginator.page(paginator.num_pages)
     return page
 
 
-def quests_list_all(request):
-    quests_list = Question.objects.new()
-    quests = paginate_question(request, quests_list)
-    return render(request, 'quests_list.html', {
-        'quests': quests,
+def questions_list_all(request):
+    questions_list = Question.objects.new().order_by('-id')
+    questions = paginate(request, questions_list)
+    return render(request, 'questions_list.html', {
+        'questions': questions,
     })
 
 
 def quests_list_popular(request):
-    quests = Question.objects.popular()
-    page = request.GET.get('page', 1)
-    paginator = Paginator(quests, 10)
-    paginator.baseurl = '/?page='
-    page = paginator.page(page)
-    return render(request, 'quests_list.html', {
-        'quests': page.object_list,
-        'paginator': paginator,
-        'page': page,
+    questions = Question.objects.popular()
+    questions = paginate(request, questions)
+    return render(request, 'popular_questions.html', {
+        'questions': questions,
     })
 
 
 def get_quest(request, id):
-    quest = get_object_or_404(Question, pk=id)
+    question = get_object_or_404(Question, pk=id)
     return render(request, 'quests.html', {
-        'quest': quest
+        'question': question
     })
