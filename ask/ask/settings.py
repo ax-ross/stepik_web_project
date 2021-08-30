559cd58d6982fa7d8bf8b35ddefb4b9c9d39a8cc
ax-ross <ax-ross@yandex.ru>
2021-08-30 13:26:12 +0300

    ready for deploy

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -78,8 +78,9 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR + '/' 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db',
+        'USER': 'root',
     }
 }
 
@@ -125,3 +126,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/?page=1'
